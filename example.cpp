6c2cb4e7ba699a298cd5dd7975ff50202a76c2ad
Abhinavthakur10 <161066057+Abhinavthakur10@users.noreply.github.com>
2024-08-06 08:13:03 +0530

    basic of 2d array

---
+++ example.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void reversestring(string st){
+    int start=0;
+    int end=st.length()-1;
+    while(s<e){
+        swap(st[start++],st[end--])
+    }
+
+string reversewords(string st){
+    int start=0;
+    int end=
+    string ss="";
+    while(start== '\0'){
+
+        if(st[start]=' ' && st[start=1]=' '){
+            start++;
+        }
+
+        for(int i=0; i != ' '; i++){
+            ss.push_back(st[i]);
+        }
+    }
+    
+}
+
+}
+int main(){
+    string st= "the sky is blue";
+    reversestring(st);
+
+
+}
