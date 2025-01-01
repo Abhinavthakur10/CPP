a048c66c7804b94efa5872b55c2fb12d78464f3c
Abhinavthakur10 <161066057+Abhinavthakur10@users.noreply.github.com>
2025-01-01 16:56:00 +0530

    reference variable

---
+++ memory/reference_variable.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+/*
+int main(){
+
+    int i=5;
+     
+    // create reference variable 
+
+
+    int &j=i;
+    // here, j and i have same memory address so both have same value for increment and decrement 
+
+    cout << i << endl;
+    i++;
+    cout << j << endl; 
+    j++;                      
+    cout<< i << endl;
+    cout<< j << endl;
+
+}
+*/
+void update2( int &j){ // PASS BY REFRENCE
+    j++;
+}
+
+void update1(int n){ // this is PASS BY VALUE
+     n++;
+}
+
+int main(){
+     
+     int n=5; 
+     cout<< "Before : " << n << endl;
+
+     update1(n);
+
+     cout<< "after : " << n  << endl;
+}
--- pointers/double_pointer.cpp
+++ pointers/double_pointer.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+/*
 int main(){
 
     int i=5;
@@ -11,5 +12,38 @@ int main(){
     cout<< "address of p "<< &p <<endl;
 
     cout<< *p2 << endl;
+
     
+    
+}
+*/
+
+void update(int **p2){
+
+    /* p2= p2+1; // it will change nothing in main function because it is function pass by vlaue. */
+
+    /* *p2= *p2+1;  // it will increment in the value of p as it is not present in the update fucntion */
+
+    **p2=**p2 +1 ;  // it will increment the value of i in  main fucntion
+
+}
+
+int main(){
+
+    int i=5;
+    int *p=&i;
+    int **p2=&p;   // double pointer
+
+    cout<< "before "<< i <<endl;
+    cout<< "before"<< p << endl;
+    cout<< "before"<< p2 <<endl;
+
+    update(p2);
+
+    cout<< "after "<< i <<endl;
+    cout<< "after"<< p << endl;
+    cout<< "after"<< p2 <<endl;
+
+
+
 }
