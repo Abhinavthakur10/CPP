0df99639d9400a8904157f0f086a8ea15f880275
Abhinavthakur10 <161066057+Abhinavthakur10@users.noreply.github.com>
2025-01-14 09:23:17 +0530

    fabonaci series

--- Recursion/recursion_1.cpp
+++ Recursion/recursion_1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+/*
 int factorial(int n){
 
     // Base condition
@@ -29,3 +30,29 @@ int main(){
     cout<< "Factorial of " << n << " is : " << ans<< endl;
     
 }
+*/
+
+/////////////////////// fibonaci series ///////////
+
+int fib(int n){
+    if (n==0)
+        return 0;
+
+    if (n==1)
+        return 1;
+
+    return fib(n-2)+ fib(n-1);
+}
+
+int main(){
+    int n;
+    cin>> n;
+
+    int ans = fib(n);
+
+    cout<< "Your fibonaci number is : " << ans << endl;
+
+}
+
+
+/////////////////////////
